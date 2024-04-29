f58f4489cbf5d12f561bfcb6c2952d0c5587a31b
Aidan <aizimmerman03@gmail.com>
2024-04-28 18:37:24 -0700

    Added viewer class

---
+++ viewer.py
@@ -0,0 +1,14 @@
+class Viewer:
+    
+    def display_menu(self, menu):
+        print(menu)
+    def show_day(self,day):
+        print(day)
+    def show_week(self,week):
+        print(week)
+    def show_month(self,month):
+        print(month)
+    def show_all(self, allTasks):
+        print(allTasks)
+    def show_task_values(self, taskValues):
+        print(taskValues)
